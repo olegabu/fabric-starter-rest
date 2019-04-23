7e1fa59b7eb27a14e8afc743864f82dfb789c35c
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-04-23 16:58:42 +0300

    deploy webapps fix

--- web-app-manager.js
+++ web-app-manager.js
@@ -11,7 +11,7 @@ function unlinkFile(path) {
     try {
         fs.unlinkSync(path);
     } catch (e) {
-        logger.warn(`Error deleting file ${fileObj.path}`, e);
+        logger.warn(`Error deleting file ${path}`, e);
     }
 }
 
@@ -22,7 +22,7 @@ class WebAppManager {
         const readStream = fs.createReadStream(fileObj.path);
         const extractPath = cfg.WEBAPPS_DIR;
         if (!fs.existsSync(cfg.WEBAPPS_DIR)){
-            fs.mkdir(cfg.WEBAPPS_DIR);
+            fs.mkdirSync(cfg.WEBAPPS_DIR);
         }
         return new Promise((resolve, reject)=>{
             const pipe = readStream.pipe(unzip.Extract({path: extractPath})); //TODO: check if folder inside zip have different name
