00f69a2e93a75831c881edbfc358a5a2e1c68557
leonidle <>
2020-11-06 00:43:44 +0300

    allowed orgs fix

--- service/integration-service.js
+++ service/integration-service.js
@@ -35,7 +35,7 @@ class IntegrationService {
         const allowedOrg = await this.checkOrgIsAllowed(org);
         const defaultClient = await this.getDefaultClient();
         const result = defaultClient.addOrgToChannel(cfg.DNS_CHANNEL, org);
-        _.set(allowedOrg, "joined", true);
+        allowedOrg.joined=true;
         return result;
     }
 
@@ -47,6 +47,7 @@ class IntegrationService {
 
     async integrateOrderer(orderer) {
         const allowedOrg = await this.checkOrgIsAllowed(orderer);
+        logger.debug("integrateOrderer accepting:", allowedOrg);
         const defaultClient = await this.getDefaultClient();
         const result = await osnManager.OsnManager.addRaftConsenter(orderer, defaultClient);
         _.set(allowedOrg, "ordererJoined", true);
@@ -55,12 +56,13 @@ class IntegrationService {
 
     async checkOrgIsAllowed(org) {
         const allowedOrg = this.orgsToAccept[org.orgId];
-        logger.info("Check for org presents in the accept list:", allowedOrg);
+        logger.info("Check org is in accepted:", allowedOrg, "ACCEPT_ALL_ORGS:", cfg.ACCEPT_ALL_ORGS);
         if (cfg.ACCEPT_ALL_ORGS ||
             (allowedOrg && !allowedOrg.joined
                 && org.orgIp == allowedOrg.orgIp
                 && org.domain == allowedOrg.domain
                 && org.tlsCert == allowedOrg.tlsCert)) {
+            log.debug("checkOrgIsAllowed: accepting ", allowedOrg);
             return allowedOrg || org;
         }
         const errMessage = `Org ${org.orgId} is not allowed`;
