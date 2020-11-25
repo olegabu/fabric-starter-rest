7c1a73bb25e5847547db70e5845672d26486543a
leonidle <>
2020-11-25 12:53:59 +0300

    fix network name

--- examples/webapp/appstore/demo-jar-app/build.sh
+++ examples/webapp/appstore/demo-jar-app/build.sh
@@ -4,6 +4,7 @@ appSrcPath=../../jar-webapp
 pushd ${appSrcPath}
 mvn package
 popd
+mkdir -p app
 cp ${appSrcPath}/target/demo-jar-webapp-0.1-jar-with-dependencies.jar app/
 rm demo-jar-app.*
 #zip -r demo-jar-app.zip .
