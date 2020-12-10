797fb8ca9cbb2605c42e2a2030fb77aee634530e
leonidle <>
2020-12-10 16:01:03 +0300

    tls example

--- examples/webapp/jar-webapp/src/main/java/com/baeldung/jetty/JettyServer.java
+++ examples/webapp/jar-webapp/src/main/java/com/baeldung/jetty/JettyServer.java
@@ -1,9 +1,10 @@
 package com.baeldung.jetty;
 
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 class JettyServer {
@@ -25,7 +26,22 @@ class JettyServer {
         QueuedThreadPool threadPool = new QueuedThreadPool(maxThreads, minThreads, idleTimeout);
 
         server = new Server(threadPool);
-        ServerConnector connector = new ServerConnector(server);
+
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStoreResource(Resource.newClassPathResource("keystore.jks"));
+        sslContextFactory.setKeyStorePassword("123456");
+        sslContextFactory.setKeyManagerPassword("123456");
+
+        HttpConfiguration httpsConfiguration = new HttpConfiguration();
+        SecureRequestCustomizer secureRequestCustomizer = new SecureRequestCustomizer();
+        httpsConfiguration.addCustomizer(secureRequestCustomizer);
+
+        ServerConnector connector = new ServerConnector(server,
+                new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+                new HttpConnectionFactory(httpsConfiguration));
+
+
+//        ServerConnector connector = new ServerConnector(server);
         connector.setPort(8080);
         server.setConnectors(new Connector[] { connector });
 
@@ -42,3 +58,21 @@ class JettyServer {
         server.stop();
     }
 }
+
+/*
+    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+    try {
+            File keystore = new ClassPathResource("keystore").getFile();
+            File truststore = new ClassPathResource("keystore").getFile();
+            connector.setScheme("https");
+            connector.setSecure(true);
+            connector.setPort(8443);
+            protocol.setSSLEnabled(true);
+            protocol.setKeystoreFile(keystore.getAbsolutePath());
+            protocol.setKeystorePass("changeit");
+            protocol.setTruststoreFile(truststore.getAbsolutePath());
+            protocol.setTruststorePass("changeit");
+            protocol.setKeyAlias("apitester");
+            return connector;
+            }*/
