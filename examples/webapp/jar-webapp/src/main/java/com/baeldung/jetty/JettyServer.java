9dbbae011007a0e3c48aa128d9be1d577cce35d4
leonidle <>
2020-12-10 18:21:37 +0300

    tls example

--- examples/webapp/jar-webapp/src/main/java/com/baeldung/jetty/JettyServer.java
+++ examples/webapp/jar-webapp/src/main/java/com/baeldung/jetty/JettyServer.java
@@ -58,20 +58,3 @@ class JettyServer {
     }
 }
 
-/*
-    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-    Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-    try {
-            File keystore = new ClassPathResource("keystore").getFile();
-            File truststore = new ClassPathResource("keystore").getFile();
-            connector.setScheme("https");
-            connector.setSecure(true);
-            connector.setPort(8443);
-            protocol.setSSLEnabled(true);
-            protocol.setKeystoreFile(keystore.getAbsolutePath());
-            protocol.setKeystorePass("changeit");
-            protocol.setTruststoreFile(truststore.getAbsolutePath());
-            protocol.setTruststorePass("changeit");
-            protocol.setKeyAlias("apitester");
-            return connector;
-            }*/
