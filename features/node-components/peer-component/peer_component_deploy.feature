ea4210433d1becebb915d2a346ac7bf8ea005f8d
leonidle <>
2021-04-19 16:28:04 +0300

    try cucumber test

---
+++ features/node-components/peer-component/peer_component_deploy.feature
@@ -0,0 +1,14 @@
+Feature: Peer component deployment
+  Deployment of peer component by backend agent
+
+  Scenario: Local Secondary peer deployment
+    Given Org name is "org1", domain is "example.com", primary host is "127.0.0.1"
+    Given Primary Orderer is running on primary host
+    Given FabricCA is running on primary host
+    Given Primary node is running on primary host
+    When User requests to start secondary peer "peer2" with PRIMARY_IP set to primary host
+    Then Peer "peer2" is enrolled to CA
+    Then Peer "peer2" is enrolled to TLSCA
+    Then DNS record for peer0 and orderer is written in _etc_hosts
+    Then Peer "peer2" is registered in DNS chaincode
+    Then Peer "peer2" container is started with corresponded ENV
---
+++ features/node-components/peer-component/step_definitions/peer_component_deploy_steps.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const {Given, When, Then} = require('cucumber');
+
+Given('Org name is {string}, domain is {string}, primary host is {string}', function (org, domain, primaryIp) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('Primary Orderer is running on {string}', function (ordererAddr) {
+
+});
+
+Given('FabricCA is running on {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('Primary node is running on {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('User requests to start secondary peer {string} with primary ip set to {string}', function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Peer {string} is enrolled to CA', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Peer {string} is enrolled to TLSCA', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('DNS record for peer0 and orderer is written in _etc_hosts', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Then('Peer {string} is registered in DNS chaincode', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Peer {string} container is started with corresponded ENV', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
