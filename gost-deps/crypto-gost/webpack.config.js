523d609276637a8132d28cbd524e579a62e51a03
leonidle <>
2022-06-23 17:26:42 +0300

    save gost

---
+++ gost-deps/crypto-gost/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+var PROD = JSON.parse(process.env.PROD_ENV || '0');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: PROD ? 'CryptoGost.min.js' : 'CryptoGost.js',
+        libraryTarget:'umd'
+    },
+    resolve:{
+        extensions:['.js','*']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      },
+    plugins:PROD ? [
+        new webpack.optimize.UglifyJsPlugin({
+            compress:{warnings:false, drop_console: true},
+            comments:false
+        })
+    ] : [],
+   
+};
